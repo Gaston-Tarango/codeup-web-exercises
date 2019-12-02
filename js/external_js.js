4eb437f41d4b817b9490f94d2884d5af1f4c7829
Gaston Tarango <gaston.tarango@gmail.com>
2019-12-02 11:08:31 -0600

    first half of exercise finished

---
+++ js/external_js.js
@@ -0,0 +1,7 @@
+console.log("Hello from our external js file!");
+
+// alert("Welcome to my Website!");
+
+var userInput = prompt("Please enter your favorite color.");
+console.log(userInput);
+alert("Great! " + userInput + " is my favorite color to!");
