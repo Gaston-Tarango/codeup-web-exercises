21418d36f3f8e71d8be9ee7ff1c0556cc43d1f1b
Gaston Tarango <gaston.tarango@gmail.com>
2019-12-09 10:49:46 -0600

    working on exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,68 +1,32 @@
-(function () {
-    "use strict"
-    /*    let showMultiplicationTable = function (number) {
-            for (let i = 1; i <= 10; i++) {
-                console.log(`${number} X ${i} = ${number * i}`);
-            }
-        };
-        showMultiplicationTable(7);*/
-
-    /*    Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
-        123 is odd
-        80 is even
-        24 is even
-        199 is odd*/
-
-    /*   let random20to200;
-       for (let i = 0; i < 10; i++) {
-           random20to200 = Math.floor(Math.random() * 200) + 1;
-           console.log(`${random20to200} is ${(random20to200 % 2 === 0) ? "even" : "odd"}`);
-       }*/
-
-
-    /*Create a for loop that uses console.log to create the output shown below.
-1
-22
-333
-4444
-55555
-666666
-7777777
-88888888
-999999999*/
-    /*    let outputArray;
-        for (let i = 0; i < 10; i++) {
-            outputArray = [];
-            for (let j = 0; j < i; j++) {
-                outputArray.push(i);
-            }
-            console.log(outputArray.join(''));
-        }*/
-    /*
-    * Create a for loop that uses console.log to create the output shown below.
-100
-95
-90
-85
-80
-75
-70
-65
-60
-55
-50
-45
-40
-35
-30
-25
-20
-15
-10
-5*/
-    /*for (let i = 100; i > 0; i-=5) {
-        console.log(i);
-    }*/
-
-
-})();
+// (function () {
+//     "use strict";
+//         let showMultiplicationTable = function (number) {
+//             for (let i = 1; i <= 10; i++) {
+//                 console.log(`${number} X ${i} = ${number * i}`);
+//             }
+//         };
+//         showMultiplicationTable(7);
+//
+//        let random20to200;
+//        for (let i = 0; i < 10; i++) {
+//            random20to200 = Math.floor(Math.random() * 200) + 1;
+//            console.log(`${random20to200} is ${(random20to200 % 2 === 0) ? "even" : "odd"}`);
+//        }
+//
+//
+//
+//         let outputArray;
+//         for (let i = 0; i < 10; i++) {
+//             outputArray = [];
+//             for (let j = 0; j < i; j++) {
+//                 outputArray.push(i);
+//             }
+//             console.log(outputArray.join(''));
+//         }
+//
+// for (let i = 100; i > 0; i-=5) {
+//         console.log(i);
+//     }
+//
+//
+// })();
