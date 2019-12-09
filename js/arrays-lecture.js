ffc4f0683fb7fd9f07916661b70edcb8db46913c
Gaston Tarango <gaston.tarango@gmail.com>
2019-12-09 14:59:41 -0600

    array lec completed

---
+++ js/arrays-lecture.js
@@ -0,0 +1,16 @@
+///DANIEL LESSON GO OVER AFTER CLASS DIDNT LEARN ANYTHING
+var pets = [];
+pets[0] = "Toby";
+pets[1] = "Princess";
+pets[2] = "Samson";
+pets[3] = "jill";
+pets[4] = "Bubbles";
+pets[5] = "Malu";
+
+
+for (var i = 0;i < pets.length;i++){
+    console.log(pets[i]);
+}
+
+
+
