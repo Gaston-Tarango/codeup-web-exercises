a9428f9bd7c07dc9b9446d7d76b6df7d99528c0d
Gaston Tarango <gaston.tarango@gmail.com>
2019-12-09 12:31:07 -0600

    completed exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,20 +1,47 @@
  "use strict";
+//  MY WAY which is wrong
+//     let askForNumber = function () {
+//         let userInput = prompt("Please enter an odd number between 1-50");
+//         if (typeof userInput === "number" && !isNaN(Number(userInput))) {
+//             return userInput;
+//         } else {
+//             return "The input you provided is not a number.";
+//         }
+//     };
+// console.log(askForNumber());
+// for (var i = 1; i <= 50; i++) {
+//
+//     if (i % 2 === 0) {
+//
+//         continue;
+//     }
+//
+//     console.log('Here is an odd number: ' + i);
+// }
 
-    let askForNumber = function () {
-        let userInput = prompt("Please enter an odd number between 1-50");
-        if (typeof userInput === "number" && !isNaN(Number(userInput))) {
-            return userInput;
-        } else {
-            return "The input you provided is not a number.";
-        }
-    };
 
-for (var i = 1; i <= 50; i++) {
+ // Vivians way 2 different problems both correct
+//problem 1
+ do {
+     var userNumber = Number(prompt("Pick an odd number between 1 and 50"));
+     if (userNumber % 2 === 0){
+         alert(userNumber + " is not odd, please pick again")
+     } else if (userNumber < 1 || userNumber > 50) {
+         alert(userNumber + " is not between 1 and 50, please pick again");
+     } else if (isNaN(userNumber)){
+         alert(userNumber + " is not a number, please pick again");
+     } else {
+         alert("congrats you picked the right number");
+         break;
+     }
+ } while (true);
 
-    if (i % 2 === 0) {
+// problem 2
+ for (var i = 1; i <= 50; i += 2){
+     if (i === userNumber){
+         console.log("yikes! Skipping number: " + i);
+         continue;
+     }
 
-        continue;
-    }
-
-    console.log('Here is an odd number: ' + i);
-}
+     console.log("Here is an odd number: " + i);
+ }
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,19 +1,43 @@
-//  "use strict";
+ "use strict";
+        // My way
 //         let showMultiplicationTable = function (number) {
 //             for (let i = 1; i <= 10; i++) {
 //                 console.log(`${number} X ${i} = ${number * i}`);
 //             }
 //         };
 //         showMultiplicationTable(7);
-//
+
+        // // Vivians solution
+        // function showMultiplicationTable(numberInput){
+        //     for (var i = 1; i <= 10; i++){
+        //         var answer = numberInput * i;
+        //
+        //         console.log(numberInput + " x " + i + " = " + answer)
+        //     }
+        // }
+        // showMultiplicationTable(7);
+
+        // my way
 //        let random20to200;
 //        for (let i = 0; i < 10; i++) {
 //            random20to200 = Math.floor(Math.random() * 200) + 1;
 //            console.log(`${random20to200} is ${(random20to200 % 2 === 0) ? "even" : "odd"}`);
 //        }
-//
-//
-//
+
+
+            // Vivians way
+ //            for (var i = 0; i <= 10; i++){
+ //                var randomNumber = Math.floor(Math.random() * 180) + 20;
+ //
+ //                if (randomNumber % 2 === 0){
+ //                    console.log(randomNumber + " is even")
+ //            } else { console.log(randomNumber + " is odd")
+ //
+ // }
+ //            }
+
+
+//     MY WAY
 //         let outputArray;
 //         for (let i = 0; i < 10; i++) {
 //             outputArray = [];
@@ -26,4 +50,13 @@
 // for (let i = 100; i > 0; i-=5) {
 //         console.log(i);
 //     }
-//
+
+
+ // Vivians Way
+ // for (var i = 1; i < 10; i++){
+ //     console.log(i.toString().repeat(i));
+ // }
+
+ // mine and vivians way
+ // for (var i = 100; i >= 5; i-=5)
+ //     console.log(i);
